f27fcb464d0e7574dcf70cdc0c581dc1e09e9481
Eric Betts <bettse@gmail.com>
2014-11-08 14:34:41 -0800

    color blending when clock 'hands' are on same pixel

--- NeoClock/NeoClock.ino
+++ NeoClock/NeoClock.ino
@@ -6,6 +6,12 @@
 #define LED_PIN 13
 #define LED_COUNT 24
 #define MAX_BRIGHTNESS 255
+#define UTC_OFFSET -8
+
+//Max of unit
+#define HOURS 12
+#define MINUTES 60
+#define SECONDS 60
 
 #define TIME_HEADER  "T"   // Header tag for serial time sync message
 #define TIME_REQUEST  7    // ASCII bell character requests a time sync message
@@ -14,11 +20,16 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, NEO_PIN, NEO_GRB + NEO_KH
 uint32_t red = strip.Color(255, 0, 0);
 uint32_t green = strip.Color(0, 255, 0);
 uint32_t blue = strip.Color(0, 0, 255);
-uint32_t magenta = strip.Color(255, 0, 255);
+uint32_t magenta = red || blue;
 uint32_t off = strip.Color(0, 0, 0);
 
+inline int positive_modulo(int i, int n) {
+  return (i % n + n) % n;
+}
 
 void setup() {
+  pinMode(LED_PIN, OUTPUT);
+
   strip.begin();
   strip.setBrightness(MAX_BRIGHTNESS/10);
   strip.show(); // Initialize all pixels to 'off'
@@ -34,22 +45,26 @@ void loop() {
     processSyncMessage();
   }
 
-  if (timeStatus()!= timeNotSet) {
+  if (timeStatus() == timeSet) {
+    digitalWrite(LED_PIN, LOW);  // LED off if synced
     for (int i = 0; i < LED_COUNT; i++) {
       strip.setPixelColor(i, off);
     }
-    strip.setPixelColor(hour() * 2, red);
-    strip.setPixelColor(minute() / 2.5, green);
-    strip.setPixelColor(second() / 2.5, blue);
-  }
 
-  if (timeStatus() == timeSet) {
-    digitalWrite(LED_PIN, HIGH); // LED on if synced
+    int hourLed = positive_modulo(hourFormat12() + UTC_OFFSET, HOURS) * (LED_COUNT / HOURS);
+    int minuteLed = minute() / (MINUTES / LED_COUNT);
+    int secondLed = second() / (SECONDS / LED_COUNT);
+
+    //Serial.println("hour: " + String(hourFormat12()) + " -> " + String(hourLed));
+
+    strip.setPixelColor(hourLed, red | strip.getPixelColor(hourLed));
+    strip.setPixelColor(minuteLed, green | strip.getPixelColor(minuteLed));
+    strip.setPixelColor(secondLed, blue | strip.getPixelColor(secondLed));
+    strip.show();
   } else {
-    digitalWrite(LED_PIN, LOW);  // LED off if needs refresh
+    digitalWrite(LED_PIN, HIGH); // LED on if needs sync
   }
 
-  strip.show();
 }
 
 void processSyncMessage() {
@@ -58,13 +73,13 @@ void processSyncMessage() {
 
   if(Serial.find(TIME_HEADER)) {
     pctime = Serial.parseInt();
-    if( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
+    if(pctime > DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
       setTime(pctime); // Sync Arduino clock to the time received on the serial port
     }
   }
 }
 
 time_t requestSync() {
-  Serial.write(TIME_REQUEST);
+  //Serial.write(TIME_REQUEST);
   return 0; // the time will be sent later in response to serial mesg
 }

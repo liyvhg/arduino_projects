d1d660db52fe28a633ae5369ed2e321e8d5946c5
Eric Betts <bettse@gmail.com>
2014-12-27 11:01:26 -0800

    shell script for calling nRFgo

---
+++ vportal/xml2service.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/Applications/PlayOnMac.app/Contents/Resources/unix/wine/bin/wine ~/Library/PlayOnMac/wineprefix/nRFGo/drive_c/Program\ Files/Nordic\ Semiconductor/nRFgo\ Studio/nRFgoStudio.exe -nrf8001 -g vportal.xml -codeGenVersion 1 -o .

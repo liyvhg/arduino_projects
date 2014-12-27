0d28393b7646600f30d1594a0de41894d7974f2e
Eric Betts <bettse@gmail.com>
2014-12-26 20:29:19 -0800

    rename file

--- vportal/blend.h
+++ vportal/blend.h
@@ -10,7 +10,7 @@
 #include <avr/interrupt.h>
 
 /* Put the nRF8001 setup in the RAM of the nRF8001.*/
-#include "vportal_services.h"
+#include "services.h"
 /* Include the services_lock.h to put the setup in the OTP memory of the nRF8001.
 This would mean that the setup cannot be changed once put in.
 However this removes the need to do the setup of the nRF8001 on every reset.*/

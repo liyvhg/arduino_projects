8e117d7fdc5aaa6151c3b6c6b07ca919b5077eca
Eric Betts <bettse@gmail.com>
2014-12-27 19:21:26 -0800

    central will connect

--- vportal/vportal.ino
+++ vportal/vportal.ino
@@ -1,3 +1,4 @@
+
 // Import libraries (BLEPeripheral depends on SPI)
 #include <SPI.h>
 #include <BLEPeripheral.h>
@@ -7,45 +8,41 @@
 #define BLE_RDY   7
 #define BLE_RST   4
 
-#define PORTAL_SERVICE_UUID_SHORT  "1530"
-#define PORTAL_SERVICE_UUID "533E15303ABEF33FCD00594E8B0A8EA3"
-#define READ_CHARACTERISTIC_UUID "533E15423ABEF33FCD00594E8B0A8EA3"
-#define WRITE_CHARACTERISTIC_UUID "533E15433ABEF33FCD00594E8B0A8EA3"
-
-
 // create peripheral instance, see pinouts above
 BLEPeripheral blePeripheral = BLEPeripheral(BLE_REQ, BLE_RDY, BLE_RST);
 
-// uuid's can be:
-//   16-bit: "ffff"
-//  128-bit: "19b10010e8f2537e4f6cd104768a1214" (dashed format also supported)
-
 // create one or more services
-BLEService service = BLEService(PORTAL_SERVICE_UUID);
+BLEService service = BLEService("533E15303ABEF33FCD00594E8B0A8EA3");
+BLEService shortService = BLEService("1530");
 
 // create one or more characteristics
-BLECharCharacteristic read_characteristic = BLECharCharacteristic(READ_CHARACTERISTIC_UUID, BLERead | BLENotify);
-BLECharCharacteristic write_characteristic = BLECharCharacteristic(WRITE_CHARACTERISTIC_UUID, BLEWrite);
+BLECharacteristic txCharacteristic = BLECharacteristic("533E15423ABEF33FCD00594E8B0A8EA3", BLENotify, 20);
+BLECharacteristic rxCharacteristic = BLECharacteristic("533E15433ABEF33FCD00594E8B0A8EA3", BLEWriteWithoutResponse, 20);
 
 void setup() {
   Serial.begin(115200);
-#if defined (__AVR_ATmega32U4__)
-  //Wait until the serial port is available (useful only for the Leonardo)
-  //As the Leonardo board is not reseted every time you open the Serial Monitor
-  while(!Serial) {}
-  delay(5000);  //5 seconds delay for enabling to see the start up comments on the serial board
-#endif
+  delay(3000);  //3 seconds delay for enabling to see the start up comments on the serial board
 
-  blePeripheral.setLocalName("Skylanders Portal\0"); // optional
-  blePeripheral.setAdvertisedServiceUuid(PORTAL_SERVICE_UUID_SHORT); // optional
+  blePeripheral.setDeviceName("Skylanders Portal\0");
+  blePeripheral.setLocalName("Skylanders Portal\0");
+  blePeripheral.setAdvertisedServiceUuid(shortService.uuid());
 
   // add attributes (services, characteristics, descriptors) to peripheral
-  blePeripheral.addAttribute(service);
-  blePeripheral.addAttribute(read_characteristic);
-  blePeripheral.addAttribute(write_characteristic);
+  //blePeripheral.addAttribute(service);
+  blePeripheral.addAttribute(shortService);
+  blePeripheral.addAttribute(rxCharacteristic);
+  blePeripheral.addAttribute(txCharacteristic);
+
+  blePeripheral.setEventHandler(BLEConnected, connectCallback);
+  blePeripheral.setEventHandler(BLEDisconnected, disconnectCallback);
+
+  txCharacteristic.setEventHandler(BLESubscribed, subscribeHandler);
+  rxCharacteristic.setEventHandler(BLEWritten, writeHandler);
 
   // begin initialization
   blePeripheral.begin();
+
+  Serial.println(F("BLE Portal Peripheral"));
 }
 
 void loop() {
@@ -58,15 +55,33 @@ void loop() {
 
     while (central.connected()) {
       // central still connected to peripheral
-      if (write_characteristic.written()) {
-        // central wrote new value to characteristic
-        Serial.println(write_characteristic.value(), DEC);
 
-      }
     }
 
     // central disconnected
     Serial.print(F("Disconnected from central: "));
     Serial.println(central.address());
   }
+
 }
+
+// callbacks
+void connectCallback(BLECentral& central) {
+  Serial.print(F("Connected event, central: "));
+  Serial.println(central.address());
+}
+
+void disconnectCallback(BLECentral& central) {
+  Serial.print(F("Disconnected event, central: "));
+  Serial.println(central.address());
+}
+
+void subscribeHandler(BLECentral& central, BLECharacteristic& characteristic) {
+  Serial.println("subscribe callback");
+}
+
+void writeHandler(BLECentral& central, BLECharacteristic& characteristic) {
+  Serial.print(F("Characteristic event, writen: "));
+  Serial.println((char*)characteristic.value());
+}
+

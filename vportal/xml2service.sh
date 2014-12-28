15713b926d284506a33af4e01b5fc6566376a604
Eric Betts <bettse@gmail.com>
2014-12-28 11:16:48 -0800

    remove shell script

--- vportal/xml2service.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/Applications/PlayOnMac.app/Contents/Resources/unix/wine/bin/wine ~/Library/PlayOnMac/wineprefix/nRFGo/drive_c/Program\ Files/Nordic\ Semiconductor/nRFgo\ Studio/nRFgoStudio.exe -nrf8001 -g vportal.xml -codeGenVersion 1 -o .

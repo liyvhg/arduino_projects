eaefbcb5f6438a639ce1d189d638906bc490c4af
Eric Betts <bettse@gmail.com>
2015-01-03 19:50:28 -0800

    Renamed 'temp' to 'thermo'

--- thermo/thermo.ino
+++ thermo/thermo.ino
@@ -1,50 +1,67 @@
 
-/*
-  The idea is to use a lilytiny to read a temperature sensor,
-  then output the temperature using red LEDs such that they
-  would approximate the look of a thermometer with red mercury.
-*/
-
-#include <Charlieplex.h>
+#include <ADC.h>
 
 #define ANALOG_MAX 1023
-#define SENSOR_PIN A1
-//3.3v reference if using teensy 3.x
-#define V_REF 2.56
+
+#define SENSOR_PIN 14
+#define LED_PIN 13
+#define V_REF 3.3
+
 //0.5V at 0 degrees C, 0.75V at 25 C, and 10mV per degree C.
 #define VOLT_AT_0C 0.5
 
-#define NUMBER_OF_PINS 4
-//define pins in the order you want to adress them
-byte pins[] = {1, 2, 3, 4};
+long previousMillis = 0;
+long interval = 1000;
 
-//initialize object
-Charlieplex charlieplex = Charlieplex(pins, NUMBER_OF_PINS);
 
-//individual 'pins' , adress charlieplex pins as you would adress an array
-charliePin led2 = { 0 , 1 };
-charliePin led3 = { 0 , 2 };
-charliePin led1 = { 1 , 2 }; //led1 is indicated by current flow from 12 to 13
+ADC *adc = new ADC(); // adc object
 
 void setup(void) {
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+
   pinMode(SENSOR_PIN, INPUT);
+  adc->setAveraging(1); // set number of averages
+  adc->setResolution(12); // set bits of resolution
+
 }
 
+
 void loop(void) {
-  int sensorValue = analogRead(SENSOR_PIN);
-  float voltage = sensorValue * V_REF / ANALOG_MAX;
+  int sensorValue = adc->analogRead(SENSOR_PIN);
+  float voltage = sensorValue * V_REF / adc->getMaxValue(ADC_0);
   //converting from 10 mv per degree with 500 mV offset
   float temperatureC = (voltage - VOLT_AT_0C) * 100 ;
-  float temperatureF = c2f(temperatureC);
+  float f = c2f(temperatureC);
 
-  charlieplex.charlieWrite(led1,HIGH);
-  charlieplex.charlieWrite(led2,HIGH);
-  charlieplex.charlieWrite(led3,HIGH);
+  unsigned long currentMillis = millis();
 
+  if(currentMillis - previousMillis > interval) {
+    previousMillis = currentMillis;
+
+    if (f < 65) {
+      //off
+    } else if (f >= 65 && f < 70) {
+      blink();
+    } else if (f >= 70 && f < 75) {
+      blink();
+      blink();
+    } else {
+      blink();
+      blink();
+      blink();
+    }
+  }
+}
+
+void blink() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(100);
+  digitalWrite(LED_PIN, LOW);
+  delay(100);
 }
 
 float c2f(float c) {
   return (c * 9.0 / 5.0) + 32.0;
 }
-
 

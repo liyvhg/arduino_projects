6643ecb5b41f76a8a6f4a8481850be4525c581ed
Eric Betts <bettse@gmail.com>
2015-01-03 22:46:01 -0800

    Fix ruby serial format

--- vportal/import.rb
+++ vportal/import.rb
@@ -1,6 +1,13 @@
 require 'rubygems'
 require 'serialport'
 
+
+class Array
+  def chr
+    self.map { |e| e.chr }
+  end
+end
+
 #params for serial port
 port_str = "/dev/tty.usbmodem1411"  #may be different for you
 baud_rate = 115200
@@ -12,23 +19,20 @@ sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
 libraryId = 1
 
-block_0 = [0xaf, 0xbe, 0xe9, 0xef, 0x17, 0x81, 0x01, 0x0f, 0xc4, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x14].to_s
-block_1 = [0xe4, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x30, 0x31, 0xa2]
+block_0 = [0xaf, 0xbe, 0xe9, 0xef, 0x17, 0x81, 0x01, 0x0f, 0xc4, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x14].chr.join
+block_1 = [0xe4, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x30, 0x31, 0xa2].chr.join
 
-
-print 'writing I command'
+puts 'writing I command'
 sp.putc('I')
-print "Arduino replied: #{sp.gets}"
 
-print "writing library id #{libraryId}"
+puts "writing library id #{libraryId}"
 sp.write(libraryId)
-print "Arduino replied: #{sp.gets}"
 
-print "writing block 0: #{block_0}"
+puts "writing block 0: #{block_0}"
 sp.write(block_0)
-print "Arduino replied: #{sp.gets}"
 
-print "writing block 1: #{block_1}"
+puts "writing block 1: #{block_1}"
 sp.write(block_1)
-print "Arduino replied: #{sp.gets}"
 
+
+sp.close

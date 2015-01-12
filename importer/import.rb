4fad28b2fbc7b1f7cf662628a425a753b55c08da
Eric Betts <bettse@gmail.com>
2015-01-11 19:52:04 -0800

    refactor import; now alphabetic

--- importer/import.rb
+++ importer/import.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require 'rubygems'
 require 'serialport'
 require 'json'
@@ -17,69 +19,65 @@ HEADER_LEN = 64
 MAX_NAME_LEN = 14 # + null character + elementAndType byte go into a single 16 byte block
 BLOCK_SIZE = 16
 
-
-def importTokens(sp, max_tokens)
+def parseJson
+  tokens = []
   File.open("backup.json", "r") do |f|
     tokens = JSON.parse(f.read)
-    tokens.each.with_index do |token, libraryId|
-      break if libraryId >= max_tokens
-      token = OpenStruct.new(token)
-      data = token.data.slice(ID_LEN, HEADER_LEN - ID_LEN - CRC_LEN)
+  end
+  tokens.collect{|token| OpenStruct.new(token) }
+end
 
-      #Replace ID
-      id = SecureRandom.hex.slice(0, ID_LEN)
-      data = id + data
+def importTokens(tokens, sp)
+  tokens.each.with_index do |token, libraryId|
+    data = token.data.slice(ID_LEN, HEADER_LEN - ID_LEN - CRC_LEN)
 
-      #Write CRC
-      bin = [data.slice(0, HEADER_LEN - CRC_LEN)].pack('H*')
-      crc = Digest::CRC16CCITT.new
-      crc.update(bin)
-      crc.finish
+    #Replace ID
+    id = SecureRandom.hex.slice(0, ID_LEN)
+    data = id + data
 
-      bin << crc.hexdigest[2..3].to_i(16)
-      bin << crc.hexdigest[0..1].to_i(16)
+    #Write CRC
+    bin = [data.slice(0, HEADER_LEN - CRC_LEN)].pack('H*')
+    crc = Digest::CRC16CCITT.new
+    crc.update(bin)
+    crc.finish
 
-      puts 'writing I command'
-      sp.putc('I')
+    bin << crc.hexdigest[2..3].to_i(16)
+    bin << crc.hexdigest[0..1].to_i(16)
 
-      puts "writing library id #{libraryId}"
-      sp.write(libraryId)
-      sleep 1
+    puts 'writing I command'
+    sp.putc('I')
 
-      block_0 = bin.slice(0, BLOCK_SIZE)
-      puts "writing block 0: #{block_0.unpack('H*')}"
-      sp.write(block_0)
+    puts "writing library id #{libraryId}"
+    sp.write(libraryId)
+    sleep 1
 
-      block_1 = bin.slice(BLOCK_SIZE, BLOCK_SIZE)
-      puts "writing block 1: #{block_1.unpack('H*')}"
-      sp.write(block_1)
+    block_0 = bin.slice(0, BLOCK_SIZE)
+    puts "writing block 0: #{block_0.unpack('H*')}"
+    sp.write(block_0)
 
-    end
+    block_1 = bin.slice(BLOCK_SIZE, BLOCK_SIZE)
+    puts "writing block 1: #{block_1.unpack('H*')}"
+    sp.write(block_1)
+
   end
 end
 
-def importNames(sp, max_tokens)
-  File.open("backup.json", "r") do |f|
-    tokens = JSON.parse(f.read)
-    puts 'writing N command'
-    sp.putc('N')
+def importNames(tokens, sp)
+  puts 'writing N command'
+  sp.putc('N')
 
-    count = [tokens.count, max_tokens].min
-    puts "write #{count} names"
-    sp.write(count)
+  puts "write #{tokens.count} names"
+  sp.write(tokens.count)
 
-    tokens.each.with_index do |token, libraryId|
-      break if libraryId >= max_tokens
-      token = OpenStruct.new(token)
-      eat = elementAndType(token)
-      #Trim if overlong
-      name = token.name.titleize.slice(0, MAX_NAME_LEN)
-      #pad if undersized
-      pad_name = name.bytes.fill(0, name.length..14)
-      bin = (pad_name + [eat]).pack('c16')
-      puts "writing: #{name} #{eat.to_s(16)}"
-      sp.write(bin)
-    end
+  tokens.each.with_index do |token, libraryId|
+    eat = elementAndType(token)
+    #Trim if overlong
+    name = token.name.titleize.slice(0, MAX_NAME_LEN)
+    #pad if undersized
+    pad_name = name.bytes.fill(0, name.length..14)
+    bin = (pad_name + [eat]).pack('c16')
+    puts "writing: #{name} #{eat.to_s(16)}"
+    sp.write(bin)
   end
 end
 
@@ -103,12 +101,13 @@ def main
   parity = SerialPort::NONE
 
   sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+  tokens = parseJson
 
-  max_tokens = 256
-  max_tokens = ARGV[0].to_i unless ARGV.empty?
+  tokens = tokens.take(ARGV[0].to_i) unless ARGV.empty?
+  tokens.sort_by!(&:name)
 
-  importNames(sp, max_tokens)
-  importTokens(sp, max_tokens)
+  importNames(tokens, sp)
+  importTokens(tokens, sp)
 
   sp.close
 end

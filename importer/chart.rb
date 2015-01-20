c15448a53107159ab54251f7a73fb05c9591de14
Eric Betts <bettse@gmail.com>
2015-01-19 20:55:46 -0800

    ruby script to chart token types

---
+++ importer/chart.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'json'
+require 'digest/crc16_ccitt'
+require 'securerandom'
+require 'active_support/core_ext/string/inflections'
+
+ID_LEN = 8
+CRC_LEN = 4
+HEADER_LEN = 64
+MAX_NAME_LEN = 14 # + null character + elementAndType byte go into a single 16 byte block
+BLOCK_SIZE = 16
+
+def parseJson
+  tokens = []
+  File.open("backup.json", "r") do |f|
+    tokens = JSON.parse(f.read)
+  end
+  tokens.each do |token|
+    token['name'] = token['name'].titleize
+    token['id'] = "#{token['data'][34..35]}#{token['data'][32..33]}"
+  end
+
+  tokens.collect{|token| OpenStruct.new(token) }
+end
+
+def main
+  tokens = parseJson
+  tokens.sort_by!(&:id)
+  tokens.each do |token|
+    puts "#{token.id}\t\t#{token.element}\t\t#{token.type}\t\t#{token.name}"
+  end
+end
+
+main

84c7de69af5ba9153bc0c0de81483e8087161652
Eric Betts <bettse@gmail.com>
2015-01-20 18:41:03 -0800

    integer ids

--- importer/chart.rb
+++ importer/chart.rb
@@ -19,7 +19,7 @@ def parseJson
   end
   tokens.each do |token|
     token['name'] = token['name'].titleize
-    token['id'] = "#{token['data'][34..35]}#{token['data'][32..33]}"
+    token['id'] = 16 * token['data'][34..35].to_i(16) + token['data'][32..33].to_i(16)
   end
 
   tokens.collect{|token| OpenStruct.new(token) }
@@ -29,7 +29,7 @@ def main
   tokens = parseJson
   tokens.sort_by!(&:id)
   tokens.each do |token|
-    puts token.id.ljust(10) + token.element.to_s.ljust(15) + token.type.to_s.ljust(15) + token.name
+    puts token.id.to_s.ljust(10) + token.element.to_s.ljust(15) + token.type.to_s.ljust(15) + token.name
   end
 end
 
